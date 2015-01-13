f1eea18740e6084a875d8de21661298c8415c118
AJ Diaz <ajdiazwriter@yahoo.com>
2015-01-13 03:21:02 -0800

    added epic sign in and login stuff

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+#set to hostname
+#for devise
+  config.action_mailer.default_url_options = { host: 'onlythebestmemes.com'}
 end
