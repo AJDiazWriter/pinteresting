907314f3c23de226af37abb9f5877615354e5757
AJ Diaz <ajdiazwriter@yahoo.com>
2015-01-24 13:27:13 -0800

    Added names and validations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150114060741) do
+ActiveRecord::Schema.define(version: 20150124210108) do
 
   create_table "pins", force: :cascade do |t|
     t.string   "description"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20150114060741) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
